1647ff8e9cb098d3283c30e176b523742c3bd4bd
Basilio Herimiharisoa <basilioherimiharisoa79@gmail.com>
2025-02-24 17:13:49 +0300

    Update Method

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ const ItemCtrl = (function() {
     };
 
     return {
-        getItems: function() {
-            return data.items;
-        },
-        getTotalTask: function() {
-            return data.totalTask();
-        },
         addItem: function(task) {
             let ID = 0;
             if (data.items.length > 0) {
@@ -30,8 +24,35 @@ const ItemCtrl = (function() {
             const newItem = new Item(ID, task);
             data.items.push(newItem);
         },
+        findItemById: function(id) {
+            let itemFound = "";
+            for (const item of data.items) {
+                if (id == item.id) {
+                    itemFound = item;
+                    break;
+                }
+            }
+            return itemFound;
+        },
+        getItems: function() {
+            return data.items;
+        },
+        getTotalTask: function() {
+            return data.totalTask();
+        },
         logData: function() {
             return data;
+        },
+        setCurrentItem: function(item) {
+            data.currentItem = item;
+        },
+        updateItem: function(newItem){
+            for (const item of data.items) {
+                if (data.currentItem.id == item.id) {
+                    item.name = newItem;
+                    break;
+                }
+            }
         }
     }
 })();
@@ -39,12 +60,28 @@ const ItemCtrl = (function() {
 const UICtrl = (function() {
     const UISelector = {
         addBtn: "#add-btn",
+        editBtn: '#edit-btn',
+        editConteneur: '#edit-none',
         input: "#input-todo",
         nombreTache: "#tasks-nombre",
         ul: "#items-collection"
     };
 
     return {
+        editInput: function(item) {
+            document.querySelector(UISelector.input).value = item.name;
+        },
+        editStatement: function() {
+            if (document.querySelector(UISelector.editBtn).classList.contains('hidden')) {
+                document.querySelector(UISelector.addBtn).classList.add('hidden');
+                document.querySelector(UISelector.editConteneur).classList.add('hidden');
+                document.querySelector(UISelector.editBtn).classList.remove('hidden');
+            } else {
+                document.querySelector(UISelector.addBtn).classList.remove('hidden');
+                document.querySelector(UISelector.editConteneur).classList.remove('hidden');
+                document.querySelector(UISelector.editBtn).classList.add('hidden');
+            }
+        },
         clearInput: function() {
             document.querySelector(UISelector.input).value = "";
         },
@@ -69,10 +106,10 @@ const UICtrl = (function() {
                         </div>                     
                         <div class="flex items-center gap-3">
                             <button class="text-blue-500 hover:text-blue-700 text-xl transition-transform transform hover:scale-110">
-                                <i class="fa fa-pencil"></i>
+                                <i class="edit-action fa fa-pencil"></i>
                             </button>
                             <button class="text-red-500 hover:text-red-700 text-xl transition-transform transform hover:scale-110">
-                                <i class="fa fa-trash"></i>
+                                <i class="delete-action fa fa-trash"></i>
                             </button>
                         </div>
                     </li>
@@ -87,30 +124,61 @@ const UICtrl = (function() {
 const App = (function(ItemCtrl, UICtrl) {
     const loadEvent = function() {
         const UISelector = UICtrl.getSelectors();
-
-        document.querySelector(UISelector.addBtn).addEventListener('click', function(e) {
-            const inputValue = UICtrl.getInputValue().task;
-            if (inputValue !== "") {
-                ItemCtrl.addItem(inputValue);
-
-                UICtrl.clearInput();
-
-                const items = ItemCtrl.getItems();
-                const nombreTache = ItemCtrl.getTotalTask();
-                UICtrl.populateItems(items, nombreTache);
-            }
-            
-            e.preventDefault();
-        });
+        //Create
+        document.querySelector(UISelector.addBtn).addEventListener('click', addItemSubmited);
+        //Read
+        document.addEventListener('DOMContentLoaded', readAllItems);
+        //Update
+        document.querySelector(UISelector.ul).addEventListener('click', addItemToForm);
+        document.querySelector(UISelector.editBtn).addEventListener('click', updateItem);
     }
+
+    const addItemSubmited = function(e) {
+        const inputValue = UICtrl.getInputValue().task;
+        if (inputValue !== "") {
+            ItemCtrl.addItem(inputValue);
+            UICtrl.clearInput();
+            readAllItems();
+        }
+        e.preventDefault();
+    };
+
+    const readAllItems = function() {
+        
+        const items = ItemCtrl.getItems();
+        const nombreTache = ItemCtrl.getTotalTask();
+        UICtrl.populateItems(items, nombreTache);
+
+    };
+
+    const addItemToForm = function(e) {
+        if (e.target.classList.contains('edit-action')) {
+            const listId = e.target.parentNode.parentNode.parentNode.id;
+            const listIdArr = listId.split('-');
+            const id = parseInt(listIdArr[1]);
+            const editItem = ItemCtrl.findItemById(id);
+            ItemCtrl.setCurrentItem(editItem);
+            UICtrl.editInput(editItem);         
+            UICtrl.editStatement();
+        }
+        e.preventDefault();
+    };
+
+    const updateItem = function(e) {
+        const inputValue = UICtrl.getInputValue().task;
+        if (inputValue !== "") {
+            ItemCtrl.updateItem(inputValue);
+            UICtrl.clearInput();
+            UICtrl.editStatement();
+            readAllItems();
+        }
+        e.preventDefault();
+    };
+
+
     return {
         init: function() {
             console.log('Initializing App...');
-
-            const items = ItemCtrl.getItems();
-            const nombreTache = ItemCtrl.getTotalTask();
-            UICtrl.populateItems(items, nombreTache);
-
             loadEvent();
         }
     }
